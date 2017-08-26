ecfa5772a91c051a105986e69d8bb3bcab8fd5e5
FranciscoDA <franciscoda@outlook.com>
2017-08-26 02:32:42 -0300

    parsear prueba.txt y mostrar debug

--- src/compilador/Main.java
+++ src/compilador/Main.java
@@ -1,7 +1,32 @@
 package compilador;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java_cup.runtime.Symbol;
 
 public class Main {
+	private final static Path PRUEBA_PATH = Paths.get("prueba.txt");
 	public static void main(String[] argv) {
-		System.out.println("Hola mundo");
+		try {
+			BufferedReader br = Files.newBufferedReader(PRUEBA_PATH);
+			Scanner sc = new Scanner(br);
+			try {
+				Symbol s = sc.debug_next_token();
+				while (s.sym != sym.EOF) {
+					System.out.println("Sym: " + s.toString());
+					s = sc.debug_next_token();
+				}
+			}
+			catch (RuntimeException ex) {
+				ex.printStackTrace();
+			}
+		}
+		catch (IOException ex) {
+			System.err.println("No se pudo abrir el archivo de prueba " + PRUEBA_PATH.toString());
+		}
+		
 	}
 }
